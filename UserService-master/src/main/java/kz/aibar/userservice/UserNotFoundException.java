7458ec3fbee0f43960f26efe69b8fd26cb81f47f
AibarIbray <aibar.s.ibrayev@gmail.com>
2023-05-12 01:08:47 +0600

    auth service updt

--- UserService-master/src/main/java/kz/aibar/userservice/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package kz.aibar.userservice;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
-

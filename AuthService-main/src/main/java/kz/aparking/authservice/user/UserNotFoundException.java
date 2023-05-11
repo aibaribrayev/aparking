54e87f7cc533f0db372e037fc070e806ac2e5cbe
AibarIbray <aibar.s.ibrayev@gmail.com>
2023-05-12 01:08:14 +0600

    auth-service updated

--- AuthService-main/src/main/java/kz/aparking/authservice/user/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package kz.aparking.authservice.user;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
-
